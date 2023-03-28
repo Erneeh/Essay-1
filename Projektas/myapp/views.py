1485873eaa44ee0ba36072e1baca599f96767263
Dovydas Sk <skauminasd@gmail.com>
2023-03-28 12:13:25 +0300

    Startas

--- Projektas/myapp/views.py
+++ Projektas/myapp/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 def index(request):
-    return render(request, "myapp/index.html", {})
+    return render(request, "index.html", {})
+
+
+def services(request):
+    return render(request, "boxes.html", {})
+
+
+def login(request):
+    return render(request, "login.html", {})
+
+
+def register(request):
+    form = CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, "register.html", context)
