a4810c269273fae94a47a59422bbe6db8c3dce63
Dovydas Sk <skauminasd@gmail.com>
2023-03-29 20:03:31 +0300

    Login/Register

--- Projektas/myapp/urls.py
+++ Projektas/myapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('services/', views.services, name='services'),
-    path('login/', views.login, name='login'),
+    path('login/', views.loginas, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logoutuser, name='logout'),
 ]
--- Projektas/myapp/views.py
+++ Projektas/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+
 
 def index(request):
     return render(request, "index.html", {})
@@ -10,18 +12,42 @@ def services(request):
     return render(request, "boxes.html", {})
 
 
-def login(request):
+def register(request):
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        uname = request.POST.get('uname')
+        email = request.POST.get('email')
+        password = request.POST.get('pass')
+
+        new_user = User.objects.create_user(uname, email, password)
+        new_user.first_name = fname
+        new_user.last_name = lname
+
+        new_user.save()
+        return redirect('login')
+
+    return render(request, "register.html", {})
+
+
+def loginas(request):
+    if request.method == "POST":
+        name = request.POST.get('uname')
+        password = request.POST.get('pass')
+
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('Error, user does not exist')
+
     return render(request, "login.html", {})
 
 
-def register(request):
-    form = CreateUserForm
+def logoutuser(request):
+    logout(request)
+    return redirect('index')
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
 
-    context = {'form': form}
-    return render(request, "register.html", context)
+
