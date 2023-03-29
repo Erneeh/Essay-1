d59e660da9d1759b0793b275797a2e6fad1e3596
Dovydas Sk <skauminasd@gmail.com>
2023-03-29 20:24:30 +0300

    Register patvarkytas, bet reikia dar tvarkyt

--- Projektas/myapp/views.py
+++ Projektas/myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib import messages
 
 
 def index(request):
@@ -19,14 +20,25 @@ def register(request):
         uname = request.POST.get('uname')
         email = request.POST.get('email')
         password = request.POST.get('pass')
+        password2 = request.POST.get('pass2')
 
-        new_user = User.objects.create_user(uname, email, password)
-        new_user.first_name = fname
-        new_user.last_name = lname
+        if password == password2:
+            if User.objects.filter(username=uname).exists():
+                return HttpResponse('Error, vartotojo vardas uzimtas')
+            else:
+                if User.objects.filter(email=email).exists():
+                    return HttpResponse('Error, el. pastas uzimtas')
+                else:
+                    new_user = User.objects.create_user(uname, email, password)
+                    new_user.first_name = fname
+                    new_user.last_name = lname
 
-        new_user.save()
-        return redirect('login')
-
+                    new_user.save()
+                    messages.info(request, f"Vartotojas {uname} sukurtas")
+                    return redirect('login')
+        else:
+            messages.error(request, "Slaptazodziai nesutampa")
+            return redirect("register")
     return render(request, "register.html", {})
 
 
@@ -48,6 +60,3 @@ def loginas(request):
 def logoutuser(request):
     logout(request)
     return redirect('index')
-
-
-
