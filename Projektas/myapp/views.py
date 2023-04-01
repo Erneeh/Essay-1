1edbff2275fb21f5a34c2866171337f1e2c99575
erne.undze@gmail.com <erne.undze@gmail.com>
2023-04-01 17:03:14 +0300

    n
    nn

--- Projektas/myapp/views.py
+++ Projektas/myapp/views.py
@@ -75,8 +75,8 @@ api_key = os.getenv("OPENAI_KEY", None)
 def paklausk(request):
     chatbot_response = None
     if request.method == "POST":
-        kontentas = "You are Lithuanian writer, try to provide information as accurately" \
-                    " as possible in Lithuania language"
+        kontentas = "You are Lithuanian named 'Essay.lt žinių meistras', try to provide information as accurately" \
+        " as possible in Lithuanian language including every subject from a-z, aswell make sure don't make mistakes"
 
         openai.api_key = api_key
         user_input = request.POST.get("user_input")
@@ -101,7 +101,7 @@ def rasiniai(request):
     if request.method == "POST":
         openai.api_key = api_key
         user_input = request.POST.get("user_input")
-        kontentas = "You are Lithuanian writer," \
+        kontentas = "You are Lithuanian writer named 'Essay.lt rašytojas'," \
                     " try to provide information as accurately as possible in Lithuania language," \
                     " you dont answer other questions that are not related to anything " \
                     "else but writing essays/letters/poems etc.. " \
@@ -116,7 +116,7 @@ def rasiniai(request):
                 {"role": "user", "content": user_input}
             ],
 
-            temperature=1
+            temperature=0.3
         )
         chatbot_response = response['choices'][0]['message']['content']
 
