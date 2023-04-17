888778346b1582cd74dee1b07454ce7986644a97
erne.undze@gmail.com <erne.undze@gmail.com>
2023-04-17 23:47:08 +0300

    patvarkiau responsive dizaina

--- Projektas/myapp/urls.py
+++ Projektas/myapp/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
                   path('anglu/', views.anglu, name='anglu'),
                   path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
                   path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-                  path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
-                       name='password_reset_confirm'),
+                  path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
                   path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
                   path('planai/', views.subscription, name='planai'),
 
