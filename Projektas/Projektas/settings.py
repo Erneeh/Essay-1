eb9b7af98221d35796c4a9397fb02826814bd07c
erne.undze@gmail.com <erne.undze@gmail.com>
2023-04-28 13:57:24 +0300

    Update settings.py

--- Projektas/Projektas/settings.py
+++ Projektas/Projektas/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/public/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = '/usr/local/lsws/Example/html/Essay/Projektas/public/static'
 STATICFILES_DIRS = os.path.join(BASE_DIR, '/static/'),
 
