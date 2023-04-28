39c36c1b339ca7cd17cdb3d57e359413dd508229
erne.undze@gmail.com <erne.undze@gmail.com>
2023-04-28 13:46:16 +0300

    changtes root

--- Projektas/Projektas/settings.py
+++ Projektas/Projektas/settings.py
@@ -112,9 +112,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/python/static/'
+STATIC_ROOT = '/usr/local/lsws/Example/html/demo/public/static'
 STATICFILES_DIRS = os.path.join(BASE_DIR, '/static/'),
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
