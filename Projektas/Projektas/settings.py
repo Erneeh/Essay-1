884f8f4b33ee3dab9ad047e26b81519f85d99c71
Dovydas Sk <skauminasd@gmail.com>
2023-04-28 20:04:02 +0300

    test

--- Projektas/Projektas/settings.py
+++ Projektas/Projektas/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'Projektas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': "/usr/local/lsws/Example/html/Essay/Projektas/db.sqlite3",
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
