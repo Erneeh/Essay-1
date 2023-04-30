d85406fb3d54b391343fbc245973987b8c6e8a95
erne.undze@gmail.com <erne.undze@gmail.com>
2023-04-30 15:13:00 +0300

    Update settings.py

--- Projektas/Projektas/settings.py
+++ Projektas/Projektas/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)a5t2&#*m08-8w@6uiz+j1xtd^bdiaegh2+aa&+&1@=@(sl00q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
